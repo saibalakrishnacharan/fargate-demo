3afb30d8dcfc0028da81169f0e9f778cc97e2e42
saibalakrishnacharan <saibalakrishna7799@gmail.com>
2024-08-06 22:25:03 +0530

    first commit

---
+++ src/main/java/com/example/fargate_demo/FargateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fargate_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FargateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FargateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fargate_demo/FargateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fargate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FargateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
